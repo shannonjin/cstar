b26a40c8fa9943f137bf53d97a6e4b8e7ecac16f
shannonjin <shannonj112@gmail.com>
2021-12-17 03:46:49 -0500

    wtf

--- codegen.ml
+++ codegen.ml
@@ -3,7 +3,7 @@
  
  let translate decl_lst =
    let context = L.global_context() in
-   let the_module = L.create_module context "Rippl" in
+   let the_module = L.create_module context "Cstar" in
    (* int, char, bool, float, void *)
    let i32_t      	 = L.i32_type    context
      and i8_t       = L.i8_type     context
@@ -12,14 +12,14 @@
  
    let ltype_of_typ = function
        Int   -> i32_t
+     | Char -> i8_t
      | Bool  -> i1_t
      | Float -> float_t
-     | Char -> i8_t
+  
      in
  
- 
      let print_main vd = match vd with
-       | (Vdef ("main", ListLit(CharLit(c) :: xs))) -> 
+       | (Modit ("main",[])) -> 
          let printf_t : L.lltype = 
            L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
        let printf_func : L.llvalue = 
@@ -28,10 +28,10 @@
          let main_f = L.define_function name main_t the_module in *)
          let builder = L.builder_at_end context (L.entry_block printf_func) in
          let char_format_str = L.build_global_stringptr "%c\n" "fmt" builder in
-         let l_char = L.const_int i8_t (Char.code c) in
+         let l_char = L.const_int i8_t (Char.code c) in 
          L.build_call printf_func [| char_format_str ; l_char |] "printf" builder;
          L.build_ret_void builder
-       | _ -> raise (Failure "NO")
+       | _ -> raise (Failure "YIKES")
      in 
      print_main (List.hd decl_lst);
      the_module
